28eae7c5540a372330897509f5f3eaebe4a44c88
Brandon <Brandnamecarr@ufl.edu>
2022-03-18 13:50:44 -0400

    bs4 attempt

--- worlde.py
+++ worlde.py
@@ -1,39 +1,37 @@
-# World Bot
-import re
+# Wordle Bot
+import requests
+from bs4 import BeautifulSoup
+import pyautogui
 
 # CONSTANTS
-WORDLE_CHARS = []  # can store correct matches in a tuple:
-MISPLACED_CHARS = []
+WORD_BANK = []
 GUESSES = 6
+GAME_URL = "https://www.nytimes.com/games/wordle/index.html"
 
-# compares test string with game string and see where misaligned. 
-def initialStringCompare(test, actual):
+#initalize the wordbank.
+def initLoad():
+    with open("Wordlebot\wordbank.txt", 'r') as file:
+        for line in file:
+            line = line.rstrip()        # strips word of newline.
+            WORD_BANK.append(line)
 
-    # checks for CORRECT words.
-    for i in range(0, len(test)):  # loop through the size (really, just 5)
-        if test[i] == actual[i]:
-            temp_tuple = (i, test[i])
-            WORDLE_CHARS.append(temp_tuple)
-    
-    # need to check if the character is in the word, just in the wrong spot.
-    for i in range(0, len(test)):
-        # calls helper function to return if the character is in the actual word, stores only character not index.
-        if checkForMembership(test[i], actual):
-            MISPLACED_CHARS.append(test[i])
-
-# helper function to check if a character is IN the actual word.
-def checkForMembership(character, actual):
-    if character in actual:
+def checkForMembership(s):
+    if s in WORD_BANK:
         return True
     else:
         return False
 
-# main loop for the execution
-def play():
-    while GUESSES > 0:
-        pass
+def loadPage():
+    page = requests.get(GAME_URL)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    link = soup.find('game-app')
+    for i in link:
+        test = link.find("#shadow-root")
+        print(test)
 
 # driver code
 def main():
-    play()
-    pass
+    initLoad()
+    loadPage()
+
+main()
